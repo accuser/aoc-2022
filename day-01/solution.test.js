310a719c68785a438825cc7313cda1dd1bab29c0
Matthew Gibbons <matthew@southwestkoala.co.uk>
2022-12-02 04:43:40 +0000

    Renamed test

--- day-01/solution.test.js
+++ day-01/solution.test.js
@@ -2,7 +2,7 @@ const { solve } = require("./solution");
 
 const input = `1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000\n`;
 
-describe("day 1", () => {
+describe("solution", () => {
   describe("part 1", () => {
     it("is 24000", () => {
       const { part1 } = solve(input);
