bdce045273d2756d8913bc94adb8dce06f32be0f
Matthew Gibbons <matthew@southwestkoala.co.uk>
2022-12-03 05:59:29 +0000

    Day 3

---
+++ day-03/solution.js
@@ -0,0 +1,34 @@
+const priority = (c) => c.charCodeAt(0) - (/[a-z]/.test(c) ? 96 : 38);
+
+const sum = (p, c) => p + c;
+
+const solve = (input) => {
+  const values = transform(input);
+
+  const part1 = values
+    .map((line) => [
+      line.slice(0, line.length / 2),
+      line.slice(line.length / 2),
+    ])
+    .map(([first, second]) => first.split("").find((c) => second.includes(c)))
+    .map(priority)
+    .reduce(sum);
+
+  const part2 = values
+    .reduce(
+      ([acc, arr], c, i) =>
+        i % 3 === 2 ? [[], [...arr, [...acc, c]]] : [[...acc, c], arr],
+      [[], []]
+    )[1]
+    .map(([first, second, third]) =>
+      first.split("").find((c) => second.includes(c) && third.includes(c))
+    )
+    .map(priority)
+    .reduce(sum);
+
+  return { part1, part2 };
+};
+
+const transform = (input) => input.split(/\n/).filter(Boolean);
+
+module.exports = { solve };
---
+++ day-03/solution.test.js
@@ -0,0 +1,21 @@
+const { parse, solve } = require("./solution");
+
+const input = `vJrwpWtwJgWrhcsFMMfFFhFp\njqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\nPmmdzqPrVvPwwTWBwg\nwMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\nttgJtRGJQctTZtZT\nCrZsJsPPZsGzwwsLwLmpwMDw\n`;
+
+describe("day 3", () => {
+  describe("part 1", () => {
+    it("is 157", () => {
+      const { part1 } = solve(input);
+
+      expect(part1).toBe(157);
+    });
+  });
+
+  describe("part 2", () => {
+    it("is 70", () => {
+      const { part2 } = solve(input);
+
+      expect(part2).toBe(70);
+    });
+  });
+});
