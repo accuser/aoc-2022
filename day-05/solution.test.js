3667a0fc7a9d010c588cc900fa384d3e39423801
Matthew Gibbons <matthew.gibbons@multiverse.io>
2022-12-09 14:01:56 +0000

    Paste

--- day-05/solution.test.js
+++ day-05/solution.test.js
@@ -1,6 +1,6 @@
 const { parse, solve } = require("./solution");
 
-const input = `    [D]    \n[N] [C]    \n[Z] [M] [P]\n 1   2   3 \n\nmove 1 from 2 to 1\nmove 3 from 1 to 3\nmove 2 from 2 to 1\nmove 1 from 1 to 2\n`;
+const input = `    [D]    \n[N] [C]    \n[Z] [M] [P]\n 1   2   3 \n\nmove 1 from 2 to 1\nmove 3 from 1 to 3\nmove 2 from 2 to 1\nmove 1 from 1 to 2`;
 
 describe("day 5", () => {
   describe("part 1", () => {
